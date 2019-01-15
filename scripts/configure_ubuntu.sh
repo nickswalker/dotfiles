09839082a29bd980268529f0fc4037267151a2f0
Nick Walker <nick@nickwalker.us>
2019-01-15 12:32:25 -0800

    Add LaTeX packages, screen recorder

--- scripts/configure_ubuntu.sh
+++ scripts/configure_ubuntu.sh
@@ -2,9 +2,14 @@
 
 sudo apt update
 
-sudo apt install -y chrome-gnome-shell curl dconf-cli ecryptfs-utils git lvm2 ssh sshfs tree tmux ubuntu-gnome-desktop zsh
+sudo apt install -y chrome-gnome-shell curl dconf-cli ecryptfs-utils git gtk-recordmydesktop lvm2 ssh sshfs tree tmux ubuntu-gnome-desktop zsh
 
-sudo snap install gimp inkscape slack spotify --classic
+sudo snap install gimp inkscape slack spotify sublime-text --classic
+
+# TeX Studio
+sudo add-apt-repository ppa:sunderme/texstudio
+
+sudo apt-get install texstudio
 
 sudo apt-get remove account-plugin-aim account-plugin-facebook account-plugin-flickr account-plugin-jabber account-plugin-salut account-plugin-twitter account-plugin-windows-live account-plugin-yahoo aisleriot brltty colord deja-dup deja-dup-backend-gvfs duplicity empathy empathy-common evolution-data-server-online-accounts example-content gnome-accessibility-themes gnome-contacts gnome-mahjongg gnome-mines gnome-orca gnome-screensaver gnome-sudoku gnome-video-effects gnomine landscape-common libreoffice-avmedia-backend-gstreamer libreoffice-base-core libreoffice-calc libreoffice-common libreoffice-core libreoffice-draw libreoffice-gnome libreoffice-gtk libreoffice-impress libreoffice-math libreoffice-ogltrans libreoffice-pdfimport libreoffice-presentation-minimizer libreoffice-style-galaxy libreoffice-style-human libreoffice-writer libsane libsane-common mcp-account-manager-uoa python3-uno rhythmbox rhythmbox-plugins rhythmbox-plugin-zeitgeist sane-utils shotwell shotwell-common telepathy-gabble telepathy-haze telepathy-idle telepathy-indicator telepathy-logger telepathy-mission-control-5 telepathy-salut thunderbird thunderbird-gnome-support totem totem-common totem-plugins unity-scope-audacious unity-scope-chromiumbookmarks unity-scope-clementine unity-scope-colourlovers unity-scope-devhelp unity-scope-firefoxbookmarks unity-scope-gdrive unity-scope-gmusicbrowser unity-scope-gourmet unity-scope-manpages unity-scope-musicstores unity-scope-musique unity-scope-openclipart unity-scope-texdoc unity-scope-tomboy unity-scope-video-remote unity-scope-virtualbox unity-scope-yelp unity-scope-zotero
 
